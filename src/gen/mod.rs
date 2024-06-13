97e9df86599fc6e2edabc0637e89cf0a3ed00a6a
mountainpath <mountainpath9@protonmail.com>
2024-06-13 13:34:18 +1000

    use pbjson to generate serde implementations for proto types

--- src/gen/mod.rs
+++ src/gen/mod.rs
@@ -5,21 +5,25 @@ pub mod immutable {
     pub mod common {
         pub mod v1 {
             include!("immutable.common.v1.rs");
+            include!("immutable.common.v1.serde.rs");
         }
     }
     pub mod activity {
         pub mod v1 {
             include!("immutable.activity.v1.rs");
+            include!("immutable.activity.v1.serde.rs");
         }
     }
     pub mod data {
         pub mod v1 {
             include!("immutable.data.v1.rs");
+            include!("immutable.data.v1.serde.rs");
         }
     }
     pub mod webauthn {
         pub mod v1 {
             include!("immutable.webauthn.v1.rs");
+            include!("immutable.webauthn.v1.serde.rs");
         }
     }
 }
@@ -28,21 +32,25 @@ pub mod external {
     pub mod options {
         pub mod v1 {
             include!("external.options.v1.rs");
+            include!("external.options.v1.serde.rs");
         }
     }
     pub mod activity {
         pub mod v1 {
             include!("external.activity.v1.rs");
+            include!("external.activity.v1.serde.rs");
         }
     }
     pub mod data {
         pub mod v1 {
             include!("external.data.v1.rs");
+            include!("external.data.v1.serde.rs");
         }
     }
     pub mod webauthn {
         pub mod v1 {
             include!("external.webauthn.v1.rs");
+            include!("external.webauthn.v1.serde.rs");
         }
     }
 }
@@ -50,9 +58,11 @@ pub mod external {
 pub mod google {
     pub mod api {
         include!("google.api.rs");
+        include!("google.api.serde.rs");
     }
     pub mod rpc {
         include!("google.rpc.rs");
+        include!("google.rpc.serde.rs");
     }
 }
 
@@ -61,6 +71,7 @@ pub mod services {
         pub mod public {
             pub mod v1 {
                 include!("services.coordinator.public.v1.rs");
+                include!("services.coordinator.public.v1.serde.rs");
             }
         }
     }
